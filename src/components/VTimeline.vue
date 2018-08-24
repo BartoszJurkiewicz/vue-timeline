10be5d8135b5bdd6c9813c85814b9fc5699300cd
Bartosz Jurkiewicz <bartosz.jurkiewicz@hotmail.com>
2018-08-24 14:18:55 +0200

    basic components structure

---
+++ src/components/VTimeline.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <step v-for="(stepData, index) in data" :key="index" :step-data="stepData" />
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { StepInt } from '@/interfaces/'
+import Step from './Step'
+
+@Component({
+  props: {
+    data: Array
+  },
+  components: { Step }
+})
+export default class VTimeline extends Vue {
+  data: StepInt[]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
